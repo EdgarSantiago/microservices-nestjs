9fedcea51b63c448e5a15f413f80394b2eb44f67
EdgarSantiago <edgargsp777@gmail.com>
2023-04-09 21:31:06 -0300

    swagger adicionado

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RpcExceptionFilter } from 'libs/shared/rpc-exception.filter';
 
+// security
+import helmet from 'helmet';
+import session from 'express-session';
+import * as csurf from 'csurf';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Auth')
+    .setDescription('Condominium auth')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
+  //app.use(helmet());
+  //app.use(csurf());
   await app.listen(5000);
 }
+
 bootstrap();
